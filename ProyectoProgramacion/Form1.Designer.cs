9642021a19eed770fea8ad661927ce5508224169
juan miranda <juanpitox02@hotmail.com>
2024-08-27 08:25:48 -0400

    Diseño realizado + defincion de variables

--- ProyectoProgramacion/Form1.Designer.cs
+++ ProyectoProgramacion/Form1.Designer.cs
@@ -29,108 +29,146 @@
         private void InitializeComponent()
         {
             groupBox1 = new GroupBox();
-            button1 = new Button();
-            numericUpDown1 = new NumericUpDown();
+            btnLimpiarProyecto = new Button();
+            btnGuardarProyecto = new Button();
+            numHorasTotales = new NumericUpDown();
             label4 = new Label();
-            textBox2 = new TextBox();
+            txtDescripcion = new TextBox();
             label3 = new Label();
-            textBox1 = new TextBox();
+            txtProyecto = new TextBox();
             label2 = new Label();
-            ID = new Label();
+            idProyecto = new Label();
             label0 = new Label();
             label1 = new Label();
-            button2 = new Button();
+            dataGridView1 = new DataGridView();
+            label5 = new Label();
+            groupBox2 = new GroupBox();
+            boxUsuarios = new ComboBox();
+            label9 = new Label();
+            boxArea = new ComboBox();
+            label11 = new Label();
+            boxProyecto = new ComboBox();
+            label7 = new Label();
+            btnLimpiarTarea = new Button();
+            btnGuardarTarea = new Button();
+            numHorasTarea = new NumericUpDown();
+            label6 = new Label();
+            label8 = new Label();
+            idTarea = new Label();
+            label10 = new Label();
+            txtTarea = new TextBox();
+            dataGridView2 = new DataGridView();
             groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numHorasTotales).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numHorasTarea).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
             SuspendLayout();
             // 
             // groupBox1
             // 
-            groupBox1.Controls.Add(button2);
-            groupBox1.Controls.Add(button1);
-            groupBox1.Controls.Add(numericUpDown1);
+            groupBox1.Controls.Add(btnLimpiarProyecto);
+            groupBox1.Controls.Add(btnGuardarProyecto);
+            groupBox1.Controls.Add(numHorasTotales);
             groupBox1.Controls.Add(label4);
-            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(txtDescripcion);
             groupBox1.Controls.Add(label3);
-            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(txtProyecto);
             groupBox1.Controls.Add(label2);
-            groupBox1.Controls.Add(ID);
+            groupBox1.Controls.Add(idProyecto);
             groupBox1.Controls.Add(label0);
             groupBox1.Font = new Font("Segoe Fluent Icons", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
             groupBox1.Location = new Point(12, 79);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(657, 426);
+            groupBox1.Size = new Size(657, 270);
             groupBox1.TabIndex = 1;
             groupBox1.TabStop = false;
             // 
-            // button1
+            // btnLimpiarProyecto
             // 
-            button1.Font = new Font("Segoe Fluent Icons", 14.25F);
-            button1.ForeColor = Color.Lime;
-            button1.Location = new Point(386, 304);
-            button1.Name = "button1";
-            button1.Size = new Size(102, 50);
-            button1.TabIndex = 9;
-            button1.Text = "Guardar";
-            button1.UseVisualStyleBackColor = true;
+            btnLimpiarProyecto.Font = new Font("Segoe Fluent Icons", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnLimpiarProyecto.ForeColor = Color.Red;
+            btnLimpiarProyecto.Location = new Point(531, 189);
+            btnLimpiarProyecto.Name = "btnLimpiarProyecto";
+            btnLimpiarProyecto.Size = new Size(102, 50);
+            btnLimpiarProyecto.TabIndex = 10;
+            btnLimpiarProyecto.Text = "Limpiar Campos";
+            btnLimpiarProyecto.TextAlign = ContentAlignment.TopCenter;
+            btnLimpiarProyecto.UseVisualStyleBackColor = true;
+            btnLimpiarProyecto.Click += btnLimpiarProyecto_Click;
             // 
-            // numericUpDown1
+            // btnGuardarProyecto
             // 
-            numericUpDown1.Location = new Point(170, 267);
-            numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(67, 28);
-            numericUpDown1.TabIndex = 8;
+            btnGuardarProyecto.Font = new Font("Segoe Fluent Icons", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnGuardarProyecto.ForeColor = Color.Lime;
+            btnGuardarProyecto.ImageAlign = ContentAlignment.TopCenter;
+            btnGuardarProyecto.Location = new Point(399, 189);
+            btnGuardarProyecto.Name = "btnGuardarProyecto";
+            btnGuardarProyecto.Size = new Size(102, 50);
+            btnGuardarProyecto.TabIndex = 9;
+            btnGuardarProyecto.Text = "Guardar proyecto";
+            btnGuardarProyecto.TextAlign = ContentAlignment.TopCenter;
+            btnGuardarProyecto.UseVisualStyleBackColor = true;
+            btnGuardarProyecto.Click += btnGuardarProyecto_Click;
+            // 
+            // numHorasTotales
+            // 
+            numHorasTotales.Location = new Point(181, 158);
+            numHorasTotales.Name = "numHorasTotales";
+            numHorasTotales.Size = new Size(67, 28);
+            numHorasTotales.TabIndex = 8;
             // 
             // label4
             // 
-            label4.Location = new Point(22, 267);
+            label4.Location = new Point(22, 158);
             label4.Name = "label4";
             label4.Size = new Size(123, 28);
             label4.TabIndex = 7;
             label4.Text = "Horas totales";
             // 
-            // textBox2
+            // txtDescripcion
             // 
-            textBox2.Location = new Point(170, 148);
-            textBox2.Multiline = true;
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(463, 100);
-            textBox2.TabIndex = 6;
+            txtDescripcion.Location = new Point(181, 79);
+            txtDescripcion.Multiline = true;
+            txtDescripcion.Name = "txtDescripcion";
+            txtDescripcion.Size = new Size(409, 56);
+            txtDescripcion.TabIndex = 6;
             // 
             // label3
             // 
-            label3.Location = new Point(22, 148);
+            label3.Location = new Point(22, 79);
             label3.Name = "label3";
             label3.Size = new Size(123, 28);
             label3.TabIndex = 4;
             label3.Text = "Descripción";
             // 
-            // textBox1
+            // txtProyecto
             // 
-            textBox1.Location = new Point(170, 87);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(463, 28);
-            textBox1.TabIndex = 3;
+            txtProyecto.Location = new Point(181, 29);
+            txtProyecto.Name = "txtProyecto";
+            txtProyecto.Size = new Size(277, 28);
+            txtProyecto.TabIndex = 3;
             // 
             // label2
             // 
-            label2.Location = new Point(22, 87);
+            label2.Location = new Point(22, 29);
             label2.Name = "label2";
             label2.Size = new Size(123, 28);
             label2.TabIndex = 2;
-            label2.Text = "Proyecto";
+            label2.Text = "Nombre Proyecto";
             // 
-            // ID
+            // idProyecto
             // 
-            ID.Location = new Point(81, 29);
-            ID.Name = "ID";
-            ID.Size = new Size(39, 28);
-            ID.TabIndex = 1;
-            ID.Text = "00";
+            idProyecto.Location = new Point(581, 24);
+            idProyecto.Name = "idProyecto";
+            idProyecto.Size = new Size(39, 28);
+            idProyecto.TabIndex = 1;
+            idProyecto.Text = "00";
             // 
             // label0
             // 
-            label0.Location = new Point(22, 29);
+            label0.Location = new Point(531, 24);
             label0.Name = "label0";
             label0.Size = new Size(44, 28);
             label0.TabIndex = 0;
@@ -139,52 +177,237 @@
             // label1
             // 
             label1.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(12, 9);
+            label1.Location = new Point(34, 19);
             label1.Name = "label1";
             label1.Size = new Size(657, 57);
             label1.TabIndex = 2;
             label1.Text = "Gestión de Proyectos";
             label1.TextAlign = ContentAlignment.MiddleCenter;
             // 
-            // button2
+            // dataGridView1
             // 
-            button2.Font = new Font("Segoe Fluent Icons", 14.25F);
-            button2.ForeColor = Color.Red;
-            button2.Location = new Point(531, 304);
-            button2.Name = "button2";
-            button2.Size = new Size(102, 50);
-            button2.TabIndex = 10;
-            button2.Text = "Limpiar Campos";
-            button2.UseVisualStyleBackColor = true;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(727, 79);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(609, 270);
+            dataGridView1.TabIndex = 3;
+            // 
+            // label5
+            // 
+            label5.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(12, 363);
+            label5.Name = "label5";
+            label5.Size = new Size(657, 57);
+            label5.TabIndex = 4;
+            label5.Text = "Gestión de Tareas";
+            label5.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(boxUsuarios);
+            groupBox2.Controls.Add(label9);
+            groupBox2.Controls.Add(boxArea);
+            groupBox2.Controls.Add(label11);
+            groupBox2.Controls.Add(boxProyecto);
+            groupBox2.Controls.Add(label7);
+            groupBox2.Controls.Add(btnLimpiarTarea);
+            groupBox2.Controls.Add(btnGuardarTarea);
+            groupBox2.Controls.Add(numHorasTarea);
+            groupBox2.Controls.Add(label6);
+            groupBox2.Controls.Add(label8);
+            groupBox2.Controls.Add(idTarea);
+            groupBox2.Controls.Add(label10);
+            groupBox2.Font = new Font("Segoe Fluent Icons", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            groupBox2.Location = new Point(12, 423);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(657, 283);
+            groupBox2.TabIndex = 11;
+            groupBox2.TabStop = false;
+            // 
+            // boxUsuarios
+            // 
+            boxUsuarios.FormattingEnabled = true;
+            boxUsuarios.Location = new Point(172, 159);
+            boxUsuarios.Name = "boxUsuarios";
+            boxUsuarios.Size = new Size(222, 29);
+            boxUsuarios.TabIndex = 17;
+            // 
+            // label9
+            // 
+            label9.Location = new Point(22, 159);
+            label9.Name = "label9";
+            label9.Size = new Size(144, 28);
+            label9.TabIndex = 16;
+            label9.Text = "Usuario asignado";
+            // 
+            // boxArea
+            // 
+            boxArea.FormattingEnabled = true;
+            boxArea.Items.AddRange(new object[] { "Hardware", "Redes" });
+            boxArea.Location = new Point(486, 71);
+            boxArea.Name = "boxArea";
+            boxArea.Size = new Size(147, 29);
+            boxArea.TabIndex = 15;
+            // 
+            // label11
+            // 
+            label11.Location = new Point(422, 72);
+            label11.Name = "label11";
+            label11.Size = new Size(49, 28);
+            label11.TabIndex = 14;
+            label11.Text = "Area";
+            // 
+            // boxProyecto
+            // 
+            boxProyecto.FormattingEnabled = true;
+            boxProyecto.Location = new Point(172, 71);
+            boxProyecto.Name = "boxProyecto";
+            boxProyecto.Size = new Size(222, 29);
+            boxProyecto.TabIndex = 13;
+            // 
+            // label7
+            // 
+            label7.Location = new Point(22, 31);
+            label7.Name = "label7";
+            label7.Size = new Size(144, 28);
+            label7.TabIndex = 12;
+            label7.Text = "Descripcion Tarea";
+            // 
+            // btnLimpiarTarea
+            // 
+            btnLimpiarTarea.Font = new Font("Segoe Fluent Icons", 15F);
+            btnLimpiarTarea.ForeColor = Color.Red;
+            btnLimpiarTarea.Location = new Point(531, 207);
+            btnLimpiarTarea.Name = "btnLimpiarTarea";
+            btnLimpiarTarea.Size = new Size(102, 50);
+            btnLimpiarTarea.TabIndex = 10;
+            btnLimpiarTarea.Text = "Limpiar Campos";
+            btnLimpiarTarea.UseVisualStyleBackColor = true;
+            btnLimpiarTarea.Click += btnLimpiarTarea_Click;
+            // 
+            // btnGuardarTarea
+            // 
+            btnGuardarTarea.Font = new Font("Segoe Fluent Icons", 15F);
+            btnGuardarTarea.ForeColor = Color.Lime;
+            btnGuardarTarea.Location = new Point(399, 207);
+            btnGuardarTarea.Name = "btnGuardarTarea";
+            btnGuardarTarea.Size = new Size(102, 50);
+            btnGuardarTarea.TabIndex = 9;
+            btnGuardarTarea.Text = "Guardar Tarea";
+            btnGuardarTarea.UseVisualStyleBackColor = true;
+            btnGuardarTarea.Click += btnGuardarTarea_Click;
+            // 
+            // numHorasTarea
+            // 
+            numHorasTarea.Location = new Point(172, 115);
+            numHorasTarea.Name = "numHorasTarea";
+            numHorasTarea.Size = new Size(67, 28);
+            numHorasTarea.TabIndex = 8;
+            // 
+            // label6
+            // 
+            label6.Location = new Point(22, 117);
+            label6.Name = "label6";
+            label6.Size = new Size(123, 28);
+            label6.TabIndex = 7;
+            label6.Text = "Horas";
+            // 
+            // label8
+            // 
+            label8.Location = new Point(22, 71);
+            label8.Name = "label8";
+            label8.Size = new Size(144, 28);
+            label8.TabIndex = 2;
+            label8.Text = "Proyecto vinculado";
+            // 
+            // idTarea
+            // 
+            idTarea.Location = new Point(581, 31);
+            idTarea.Name = "idTarea";
+            idTarea.Size = new Size(39, 28);
+            idTarea.TabIndex = 1;
+            idTarea.Text = "00";
+            // 
+            // label10
+            // 
+            label10.Location = new Point(531, 31);
+            label10.Name = "label10";
+            label10.Size = new Size(44, 28);
+            label10.TabIndex = 0;
+            label10.Text = "ID";
+            // 
+            // txtTarea
+            // 
+            txtTarea.Location = new Point(184, 454);
+            txtTarea.Name = "txtTarea";
+            txtTarea.Size = new Size(286, 23);
+            txtTarea.TabIndex = 11;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(727, 423);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.Size = new Size(609, 283);
+            dataGridView2.TabIndex = 12;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(681, 519);
+            ClientSize = new Size(1426, 728);
+            Controls.Add(dataGridView2);
+            Controls.Add(txtTarea);
+            Controls.Add(groupBox2);
+            Controls.Add(label5);
+            Controls.Add(dataGridView1);
             Controls.Add(label1);
             Controls.Add(groupBox1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numHorasTotales).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)numHorasTarea).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private GroupBox groupBox1;
-        private Label ID;
-        private Label label0;
+        private Label idProyecto;
         private Label label1;
         private Label label2;
-        private TextBox textBox1;
-        private TextBox textBox2;
+        private TextBox txtProyecto;
+        private TextBox txtDescripcion;
         private Label label3;
         private Label label4;
-        private Button button1;
-        private NumericUpDown numericUpDown1;
-        private Button button2;
+        private Button btnGuardarProyecto;
+        private NumericUpDown numHorasTotales;
+        private Button btnLimpiarProyecto;
+        private DataGridView dataGridView1;
+        private Label label5;
+        private Label label0;
+        private GroupBox groupBox2;
+        private Button btnLimpiarTarea;
+        private Button btnGuardarTarea;
+        private Label label6;
+        private Label label8;
+        private Label idTarea;
+        private Label label10;
+        private NumericUpDown numHorasTarea;
+        private TextBox txtTarea;
+        private Label label7;
+        private DataGridView dataGridView2;
+        private ComboBox boxProyecto;
+        private ComboBox boxArea;
+        private Label label11;
+        private ComboBox boxUsuarios;
+        private Label label9;
     }
 }
--- ProyectoProgramacion/Form1.cs
+++ ProyectoProgramacion/Form1.cs
@@ -3,8 +3,71 @@ namespace ProyectoProgramacion
     public partial class Form1 : Form
     {
         public Form1()
-        { 
+        {
             InitializeComponent();
+            limpiezaProyecto();
+            limpiezaTarea();
+        }
+        private void limpiezaProyecto()
+        {
+            /*
+             * txtProyecto set ""
+             * txtDescripcion set ""
+             * numHorasTotales set 0
+             */
+        }
+
+        private void limpiezaTarea()
+        {
+            /*
+             * txtTarea set ""
+             * boxProyecto index 0
+             * boxTarea set index 0
+             * numHorasTarea set 0
+             * boxUsuarios index 0
+             */
+        }
+
+        private void btnGuardarProyecto_Click(object sender, EventArgs e)
+        {
+            /*
+             * Gestion proyecto
+             *  proyecto        = txtProyecto
+             *  descripcion     = txtDescripcion
+             *  horas_totales   = numHorasTotales  
+             *  Para la hora utilizaremos la hora actual al guardar el objeto
+             *  
+             *
+             *  *Codigo para el guardado de los datos de objeto a la clase Proyecto
+             *
+            */
+            limpiezaProyecto(); // Limpiamos campos luego de la insercion
+        }
+
+        private void btnLimpiarProyecto_Click(object sender, EventArgs e)
+        {
+            limpiezaProyecto(); // Limpiamos los campos de Proyecto
+        }
+
+        private void btnGuardarTarea_Click(object sender, EventArgs e)
+        {
+            /*
+             * Gestion Tarea
+             *  descripcionTarea = txtTarea
+             *  proyectoAsignado = boxProyecto // Rellenar box un select de proyectos
+             *  areaAsiganada    = boxArea 
+             *  horas            = numHorasTarea
+             *  usuarioAsignado  = boxUsuarios // Rellenar box un select de usuarios
+             */
+        }
+
+        private void btnLimpiarTarea_Click(object sender, EventArgs e)
+        {
+            limpiezaTarea(); // Limpiamos campos de Tarea
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
